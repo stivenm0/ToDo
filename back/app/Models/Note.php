f2d779bc75e7982c4dc6552b8983486e91045a73
stivenm0 <estivenyeizon@gmail.com>
2024-09-30 11:58:36 -0500

    feat: 🎸 add migrations, factories and seeders

---
+++ back/app/Models/Note.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'due_date', 
+        'image',    
+    ];
+
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_note');
+    }
+
+}
